c802bbf75bac97fb0aa32e08e391e4d06c1aa23e
shy_azusa <shy.azusa@gmail.com>
2017-02-01 16:36:03 +0900

    Change reset

--- keyboards/ergodox/keymaps/shyazusa/keymap.c
+++ keyboards/ergodox/keymaps/shyazusa/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | RESET|           |   `  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |   `  |           |   `  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |   BS   |   Q  |   W  |   E  |   R  |   T  |  [   |           |   ]  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,        KC_1,    KC_2,          KC_3,  KC_4,    KC_5,    RESET,
+        KC_EQL,        KC_1,    KC_2,          KC_3,  KC_4,    KC_5,    KC_GRV,
         KC_BSPC,       KC_Q,    KC_W,          KC_E,  KC_R,    KC_T,    KC_LBRC,
         KC_LCTL,       KC_A,    KC_S,          KC_D,  KC_F,    KC_G,
         LCTL(KC_LEFT), KC_Z,    KC_X,          KC_C,  KC_V,    KC_B,    TG(1),
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: Media and mouse keys
  *
  * ,-----------------------------------------------------.           ,---------------------------------------------------.
- * |        |       |       |      |      |       |      |           |      |      |      |      |      |       |        |
+ * |        |       |       |      |      |       | RESET|           |      |      |      |      |      |       |        |
  * |--------+-------+-------+------+------+--------------|           |------+------+------+------+------+-------+--------|
  * |        | Gui+Q | Gui+W | MsUp |      | Gui+T |      |           |      | yank | Lclk | WhUp | Rclk | Past  |        |
  * |--------+-------+-------+------+------+-------|      |           |      |------+------+------+------+-------+--------|
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // MEDIA AND MOUSE
 [MDIA] = KEYMAP(
         // left hand
-        KC_TRNS,       KC_TRNS,      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,
+        KC_TRNS,       KC_TRNS,      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    RESET,
         KC_TRNS,       LGUI(KC_Q),   LGUI(KC_W), KC_MS_U, KC_TRNS, LGUI(KC_T), KC_TRNS,
         LCTL(KC_LEFT), G_A(KC_LEFT), KC_MS_L,    KC_MS_D, KC_MS_R, KC_TRNS,
         KC_TRNS,       KC_TRNS,      KC_DELT,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,
