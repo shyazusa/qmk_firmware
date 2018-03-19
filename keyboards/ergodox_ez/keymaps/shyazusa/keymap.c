133aa01738d8097f3f9bbaafbd88a11caf964ee5
shy_azusa <shy.azusa@gmail.com>
2018-03-19 11:28:40 +0900

    cosmetic change

--- keyboards/ergodox_ez/keymaps/shyazusa/keymap.c
+++ keyboards/ergodox_ez/keymaps/shyazusa/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_6,    KC_7,    KC_8,    KC_9,   KC_0,     KC_MINS,
         KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     KC_BSLS,
                  KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
-        KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  LCTL(KC_RIGHT),
+        KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  LCTL(KC_RIGHT),
                           KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_ENT,
         KC_VOLD, KC_VOLU,
         KC_SPC,
